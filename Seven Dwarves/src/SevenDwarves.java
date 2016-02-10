6c90fa826f8ef80a8d7743031e319860d916ce6c
Jumphol <Jumphol@Book>
2016-02-10 14:43:29 +0700

    SevenDwarves

---
+++ Seven Dwarves/src/SevenDwarves.java
@@ -0,0 +1,90 @@
+
+public class SevenDwarves {
+
+	public static void main(String[] args) {
+		int Dwarve1=75,Dwarve2=7,Dwarve3=6,Dwarve4=5,Dwarve5=30,Dwarve6=35,Dwarve7=4,Dwarve8=1,Dwarve9=2;
+		//check Dwarve1
+		if(Dwarve8+Dwarve9+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve1,Dwarve2 Is Fake");
+		if(Dwarve8+Dwarve2+Dwarve9+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve1,Dwarve3 Is Fake");
+		if(Dwarve8+Dwarve2+Dwarve3+Dwarve9+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve1,Dwarve4 Is Fake");
+		if(Dwarve8+Dwarve2+Dwarve3+Dwarve4+Dwarve9+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve1,Dwarve5 Is Fake");
+		if(Dwarve8+Dwarve2+Dwarve3+Dwarve4+Dwarve9+Dwarve9+Dwarve7 == 100)
+			System.out.println("Dwarve1,Dwarve6 Is Fake");
+		if(Dwarve8+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve1,Dwarve7 Is Fake");
+		if(Dwarve7+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve1,Dwarve8 Is Fake");
+		if(Dwarve7+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve8 == 100)
+			System.out.println("Dwarve1,Dwarve9 Is Fake");
+		if(Dwarve7+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve8 == 100)
+			System.out.println("Dwarve1,Dwarve9 Is Fake");
+		//check Dwarve2
+		if(Dwarve1+Dwarve8+Dwarve9+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve3 Is Fake");
+		if(Dwarve1+Dwarve8+Dwarve3+Dwarve9+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve4 Is Fake");
+		if(Dwarve1+Dwarve8+Dwarve3+Dwarve9+Dwarve9+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve5 Is Fake");
+		if(Dwarve1+Dwarve8+Dwarve3+Dwarve4+Dwarve9+Dwarve9+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve6 Is Fake");
+		if(Dwarve1+Dwarve8+Dwarve3+Dwarve4+Dwarve9+Dwarve9+Dwarve9 == 100)
+			System.out.println("Dwarve2,Dwarve7 Is Fake");
+		if(Dwarve1+Dwarve9+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve8+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve2,Dwarve9 Is Fake");
+		//check Dwarve3
+		if(Dwarve1+Dwarve2+Dwarve8+Dwarve9+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve3,Dwarve4 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve8+Dwarve9+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve3,Dwarve5 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve8+Dwarve4+Dwarve5+Dwarve9+Dwarve7 == 100)
+			System.out.println("Dwarve3,Dwarve6 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve8+Dwarve4+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve3,Dwarve7 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve7+Dwarve4+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve3,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve9+Dwarve8+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve3,Dwarve9 Is Fake");
+		//check Dwarve4
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve8+Dwarve9+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve4,Dwarve5 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve8+Dwarve5+Dwarve9+Dwarve7 == 100)
+			System.out.println("Dwarve4,Dwarve6 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve8+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve4,Dwarve7 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve7+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve4,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve8+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve4,Dwarve9 Is Fake");
+		//check Dwarve5
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve8+Dwarve9+Dwarve7 == 100)
+			System.out.println("Dwarve5,Dwarve6 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve8+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve5,Dwarve7 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve7+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve5,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve8+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve5,Dwarve9 Is Fake");
+		//check Dwarve6
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve8+Dwarve9 == 100)
+			System.out.println("Dwarve6,Dwarve7 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve7+Dwarve9 == 100)
+			System.out.println("Dwarve6,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve7+Dwarve8 == 100)
+			System.out.println("Dwarve6,Dwarve9 Is Fake");
+		//check Dwarve7
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve9 == 100)
+			System.out.println("Dwarve7,Dwarve8 Is Fake");
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve8+Dwarve6 == 100)
+			System.out.println("Dwarve7,Dwarve9 Is Fake");
+		//check Dwarve8,9
+		if(Dwarve1+Dwarve2+Dwarve3+Dwarve4+Dwarve5+Dwarve6+Dwarve7 == 100)
+			System.out.println("Dwarve8,Dwarve9 Is Fake");
+	}
+
+}
